596029487dc7711676ac9abfb947c37e8dda990c
PashaKoplyk <pashabroneboy@gmail.com>
2026-01-08 21:16:01 +0000

    modul 9

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,47 @@
 console.log('Gallery');
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const images = [
+  {
+    preview: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+    original: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+ {
+    preview:"https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__480.jpg",
+    original:"https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg",
+    description: "Cute cat",
+  },
+  {
+    preview: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202822__480.jpg",
+    original: "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202822_1280.jpg",
+    description: "Aerial Beach View",
+  },
+];
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = images
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `
+  )
+  .join("");
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,50 @@
 console.log('Form');
+
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = {
+  email: "",
+  message: ""
+};
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  emailInput.value = formData.email || "";
+  messageInput.value = formData.message || "";
+}
+
+
+function saveToLocalStorage() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  formData[name] = value.trim(); 
+  saveToLocalStorage();
+});
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+ 
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  
+  form.reset();
+  formData = { email: "", message: "" };
+  localStorage.removeItem(STORAGE_KEY);
+});
